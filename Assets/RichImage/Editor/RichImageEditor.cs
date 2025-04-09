fca515b4b527b882e5157faa302cc2c2787faa6d
musticide <mustafa25.tm@gmail.com>
2025-04-09 20:22:21 +0530

    Rich Image: initial commit

---
+++ Assets/RichImage/Editor/RichImageEditor.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(RichImage))]
+public class RichImageEditor : UnityEditor.Editor
+{
+    public void OnEnable()
+    {
+
+    }
+    public override void OnInspectorGUI()
+    {
+
+    }
+}
---
+++ Assets/RichImage/RichImage.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class RichImage : MaskableGraphic 
+{
+}
